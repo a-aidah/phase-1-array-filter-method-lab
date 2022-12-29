d184d74778fe0f397d1e59fa3d19ea7d83ff15ad
Abdullah Omer Aidah <a.o.aidah.note9@gmail.com>
2022-12-29 21:14:36 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solution here
+
+function findMatching(array, string) {
+    const result = array.filter((drivers) => drivers.toLowerCase() === string.toLowerCase());
+    return result;
+}
+
+function fuzzyMatch(array, string) {
+    const result = array.filter((drivers) => drivers.startsWith(string));
+    return result;
+}
+
+function matchName(array, string) {
+    const result = array.filter((drivers) => drivers.name === string);
+    return result;
+}
